0af17568ec2e8f8cf1b312647e639e3fa8c04760
codernishchay <nishchay.vns2000@gmaiil.com>
2022-04-26 21:28:52 -0400

    d

--- routes/paymentRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const paymentController = require('./../controller/paymentController');
-const router = express.Router();
-
-router.post('/payment', paymentController.stripePayment);
-
-module.exports = router;
