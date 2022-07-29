9cb873009a4e162c6a678ee92e2cd19fa599308d
Nishchay Verma <nishchayverma@Nishchays-MacBook-Air.local>
2022-07-29 20:23:26 +0530

    done

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -4,7 +4,7 @@ const authController = require('./../controller/authController');
 
 const router = express.Router();
 
-router.use(authController.protect);
+// router.use(authController.protect);
 router.get('/foodItem', foodController.getAllFoods);
 router.get('/foodItem/:id', foodController.getFoodById);
 router.get('/getFoodbyCategory/:category', foodController.getFoodbyCategory);
